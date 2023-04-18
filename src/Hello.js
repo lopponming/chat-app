c80260a2694a80e15b448d402a1dc1eb9e7b9cd6
lopponming <miranda.klockars@gmail.com>
2023-04-18 10:45:07 +0300

    chat app really started now
    displaying messages works, but they are gone when refreshed

--- src/Hello.js
+++ src/Hello.js
@@ -4,9 +4,10 @@ class Hello extends React.Component {
     render() {
         return (
             <div>
-                <p>Hello {this.props.name}!</p>
+                <p>hello {this.props.name}!</p>
                 <p>{this.props.name} doesn't know what's happening.</p>
-                <p>All of this is pretty cool and new to {this.props.name}.</p>
+                <p>all of this is pretty cool and new to {this.props.name}.</p>
+                <p>she might soon be {this.props.age} years old.</p>
             </div>
         );
     }
